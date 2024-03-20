098f15bacef4e0a1589f772728af5aee9f6bf54f
Ajith R <ajith8593935904@gmail.com>
2024-03-20 14:05:16 +0530

    updated file name adding numbers in begining

---
+++ 13-heap/stringSort.js
@@ -0,0 +1,35 @@
+function heapify(arr, n, i) {
+    let largest = i;
+    let left = 2 * i + 1;
+    let right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    if (largest !== i) {
+        [arr[i], arr[largest]] = [arr[largest], arr[i]];
+        heapify(arr, n, largest);
+    }
+}
+
+function heapSort(arr) {
+    let n = arr.length;
+
+    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    for (let i = n - 1; i >= 0; i--) {
+        [arr[0], arr[i]] = [arr[i], arr[0]];
+        heapify(arr, i, 0);
+    }
+}
+
+let str = "abcdefghbia";
+let sortedStr = str.split('').sort().join('');
+console.log("Sorted array is", sortedStr);
