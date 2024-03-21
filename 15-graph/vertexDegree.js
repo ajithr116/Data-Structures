e71253f523f82358721fcad9857aa1ca44180c0f
Ajith R <ajith8593935904@gmail.com>
2024-03-21 22:17:28 +0530

    workout

---
+++ 15-graph/vertexDegree.js
@@ -0,0 +1,48 @@
+
+class graph {
+    constructor(){
+        this.list = {};
+    }
+    addVertex(vertex){
+        if(!this.list[vertex]){
+            this.list[vertex] = [];
+        }
+    }
+    addEdge(vertex1,vertex2){
+        if(this.list[vertex1] && this.list[vertex2]){
+            this.list[vertex1].push(vertex2);
+            this.list[vertex2].push(vertex1);
+        }
+    }
+    removeEdge(vertex1,vertex2){
+        if(this.list[vertex1] && this.list[vertex2]){
+            this.list[vertex1] = this.list[vertex1].filter(v=>v!==vertex2);
+            this.list[vertex2] = this.list[vertex2].filter(v=>v!==vertex1);
+        }
+    }
+    removeVertex(vertex){
+        while(this.list[vertex].length){
+            const vertex2 = this.list[vertex].pop();
+            this.removeEdge(vertex,vertex2);
+        }
+        delete this.list[vertex];
+    }
+    getDegree(vertex){
+        return this.list[vertex].length;
+    }
+}
+
+const g = new graph();
+g.addVertex("a");
+g.addVertex("b");
+g.addVertex("c");
+console.log(g);
+g.addEdge("a","c")
+g.addEdge("a","b")
+g.addEdge("b","c")
+console.log(g);
+// g.removeEdge("a","c")
+// console.log(g);
+// g.removeVertex("a")
+console.log(g);
+console.log(g.getDegree("a"))
